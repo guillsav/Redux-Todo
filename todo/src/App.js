1b9427b493b42e4be63e2f161805d2647236478f
guillsav <guillsav@gmail.com>
2019-04-08 14:45:35 -0400

    addTodo action added

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,11 +1,26 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {addTodo} from './actions';
 
 import './App.css';
 
 class App extends Component {
+  addNewTodo = e => {
+    e.preventDefault();
+  };
+
   render() {
     return <div className="App">App</div>;
   }
 }
 
-export default App;
+const mapStateToProp = state => {
+  return {
+    todos: state.todos
+  };
+};
+
+export default connect(
+  mapStateToProp,
+  {addTodo}
+)(App);
--- todo/src/index.js
+++ todo/src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import rootReducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App';
+import './index.css';
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
